0567dbc89f9c9d04fa6739d5754e7091f9981a0b
kleiberd <kleiberd93@gmail.com>
2016-08-11 18:22:49 +0200

    NEW - simple admin login/logout implementation

---
+++ src/AppBundle/DataFixtures/ORM/LoadAdminData.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\Admin;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+
+class LoadAdminData implements FixtureInterface, ContainerAwareInterface
+{
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $adminUser = new Admin();
+        $adminUser->setEmail("admin@admin.com");
+        $adminUser->setUsername("admin");
+        $password = $this->container->get('security.password_encoder')->encodePassword($adminUser, "admin");
+        $adminUser->setPassword($password);
+
+        $manager->persist($adminUser);
+        $manager->flush();
+    }
+}
